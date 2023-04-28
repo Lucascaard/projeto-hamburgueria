6dff5fc7ad2bf43313562d70d3ec5d64fe13e7f7
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2023-04-28 20:46:37 -0300

    Versão Final

--- src/principal/modelos/Estoque.java
+++ src/principal/modelos/Estoque.java
@@ -2,13 +2,18 @@ package principal.modelos;
 import java.util.ArrayList;
 import java.util.List;
 
+import principal.db.Banco;
+
 public class Estoque {
 	
-		private List<ItemEstoque> itensEstoque = new ArrayList<ItemEstoque>();
 		private String enderecoEstoque;
 		
 		public void adicionarItem(ItemEstoque item) {
-			this.itensEstoque.add(item);
+			Banco.itensEstoque.add(item);
+		}
+
+		public void removerItem(ItemEstoque item) {
+			Banco.itensEstoque.remove(item);
 		}
 		
 		public Estoque() {
@@ -19,19 +24,11 @@ public class Estoque {
 			public void setEnderecoEstoque(String enderecoEstoque) {
 				this.enderecoEstoque = enderecoEstoque;
 			}
-			
-		
+				
 		//Getters
 			public String getEnderecoEstoque() {
 				return enderecoEstoque;
 			}
 
 	
-			public List<ItemEstoque> getItensEstoque() {
-				return itensEstoque;
-			}
-	
-			public void setItensEstoque(List<ItemEstoque> itensEstoque) {
-				this.itensEstoque = itensEstoque;
-			}
 	}
--- src/principal/telas/TelaEstoque.java
+++ src/principal/telas/TelaEstoque.java
@@ -4,6 +4,7 @@ import principal.util.Mensagem;
 import principal.util.Prompt;
 import principal.controles.ControleEstoque;
 import principal.controles.ControleProduto;
+import principal.db.Banco;
 import principal.modelos.ItemEstoque;
 import principal.modelos.Produto;
 
@@ -24,17 +25,25 @@ public class TelaEstoque {
         Prompt.linhaEmBranco();
         Prompt.imprimir("[1] - " + Mensagem.CREATE);
         Prompt.imprimir("[2] - " + Mensagem.READ);
-        Prompt.imprimir("[3] - " + Mensagem.VOLTAR);
+        Prompt.imprimir("[3] - " + Mensagem.UPDATE);
+        Prompt.imprimir("[4] - " + Mensagem.DELETE);
+        Prompt.imprimir("[5] - " + Mensagem.VOLTAR);
         Integer opcao = Prompt.lerInteiro();
 
             switch (opcao) {
                 case 1:
-                    TelaEstoque.cadastrar();
+                    TelaEstoque.create();
                     break;
                 case 2:
-                    TelaEstoque.listar();
+                    TelaEstoque.read();
                     break;
                 case 3:
+                    TelaEstoque.update();
+                    break;
+                case 4:
+                    TelaEstoque.delete();
+                    break;
+                case 5:
                     TelaPrincipal.mostrar();
                     break;
                 default:
@@ -46,40 +55,141 @@ public class TelaEstoque {
 
     }
 
-    public static void cadastrar(){
+    public static void create(){
 
-        //Lendo e guardando dados em variáveis
-
+        Prompt.linhaEmBranco();
         Prompt.separador();
         Prompt.imprimir(Mensagem.MSG_CADASTRO_ESTOQUE);
         Prompt.separador();
-        Integer idProduto = Prompt.lerInteiro(Mensagem.INFORME_ID);
-        Integer qtde = Prompt.lerInteiro(Mensagem.INFORME_QUANTIDADE);
 
-        if(!idProduto.equals(null)) {
+        ControleEstoque.listarId();
 
+        Integer idProduto = Prompt.lerInteiro(Mensagem.INFORME_ID_CADASTRO);
 
-            Produto produto = ControleProduto.buscarPorId(idProduto);
+        boolean control = ControleEstoque.produtoExiste(idProduto);
 
-            ItemEstoque item = new ItemEstoque();
-            item.setProduto(produto);
-            item.setQtde(qtde);
+        if(!control){
 
-            ControleEstoque.adicionar(item);
+            if(idProduto != null) {
+                
+                Produto produto = ControleProduto.buscarPorId(idProduto);
+                
+                if(produto == null){
+                    Prompt.imprimir(Mensagem.ID_INVALIDA);
+                } else {
+                    Integer qtde = Prompt.lerInteiro(Mensagem.INFORME_QUANTIDADE);
+                    ItemEstoque item = new ItemEstoque();
+                    item.setId(idProduto);
+                    item.setProduto(produto);
+                    item.setQtde(qtde);
+            
+                    ControleEstoque.adicionar(item);
+    
+                    Prompt.linhaEmBranco();
+                    Prompt.imprimir(Mensagem.PRODUTO_CADASTRADO_ESTOQUE);
+                } 
+                    
+            }
+        } else {
+
+            Prompt.linhaEmBranco();
+            Prompt.imprimir(Mensagem.PRODUTO_EXISTENTE_ESTOQUE);
         }
 
+        Prompt.linhaEmBranco();
         Prompt.pressionarEnter();
 		TelaEstoque.mostrar();
 
     }
 
+    public static void read(){
 
-    public static void listar(){
+        Prompt.linhaEmBranco();
+        Prompt.separador();
+        Prompt.imprimir(Mensagem.LISTA_ESTOQUE);
+        Prompt.separador();
+
+        Prompt.linhaEmBranco();
+
+        if(Banco.itensEstoque.isEmpty()){
+
+            Prompt.imprimir(Mensagem.ESTOQUE_VAZIO);
+        } else {
+
+            for (ItemEstoque produto : Banco.itensEstoque) {
+                 String info = "Produto: " + produto.getProduto().getNome() + "\n"
+                             + "Quantidade: " + produto.getQtde() + "\n";
+
+                Prompt.imprimir(info);
+            }
+        }
+
+        Prompt.linhaEmBranco();
+		Prompt.pressionarEnter();
+		TelaEstoque.mostrar();
+    }
+
+    public static void update(){
+
+        Prompt.linhaEmBranco();
+        Prompt.separador();
+        Prompt.imprimir(Mensagem.UPDATE_ESTOQUE);
+        Prompt.separador();
+
+        ControleEstoque.listarId();
+
+        Integer idProduto = Prompt.lerInteiro(Mensagem.INFORME_ID_ALTERAR);
+
+        if(idProduto != null){
+
+            boolean produtoExiste = ControleEstoque.produtoExiste(idProduto);
+
+            if(produtoExiste){
+
+                Integer qtde = Prompt.lerInteiro(Mensagem.QTDE_ESTOQUE);
+
+                ControleEstoque.alterarQuantidade(idProduto, qtde);
+
+                Prompt.linhaEmBranco();
+				Prompt.imprimir(Mensagem.ESTOQUE_ALTERADO);
+            } else {
+                Prompt.linhaEmBranco();
+				Prompt.imprimir(Mensagem.PRODUTO_NAO_ENCONTRADO);
+            }
+        }
+
+        Prompt.linhaEmBranco();
+		Prompt.pressionarEnter();
+		TelaEstoque.mostrar();
 
     }
 
-    public static void voltar(){
+    public static void delete(){
 
+        Prompt.linhaEmBranco();
+        Prompt.separador();
+        Prompt.imprimir(Mensagem.EXCLUIR_ESTOQUE);
+        Prompt.separador();
+
+        ControleEstoque.listarId();
+
+        Integer idProduto = Prompt.lerInteiro(Mensagem.INFORME_ID_EXCLUIR_ESTOQUE);
+
+        if(idProduto != null){
+            boolean estoqueExcluido = ControleEstoque.delete(idProduto);
+			Prompt.linhaEmBranco();
+			if(estoqueExcluido) {
+				Prompt.imprimir(Mensagem.ESTOQUE_EXCLUIDO);
+			} else {
+				Prompt.imprimir(Mensagem.PRODUTO_NAO_ENCONTRADO);
+			}
+
+		Prompt.linhaEmBranco();
+		Prompt.pressionarEnter();
+
+        }
+
+        TelaEstoque.mostrar();
     }
 
 }
